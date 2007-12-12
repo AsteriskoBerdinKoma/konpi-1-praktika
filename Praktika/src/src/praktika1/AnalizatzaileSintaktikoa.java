38035855dadf52923573998492daf9c0b84352cc
mire.lezea <mire.lezea@c955568d-1241-0410-a7ed-d13cdb9948ed>
2007-12-12 08:37:29 +0000

    ansint

---
+++ Praktika/src/src/praktika1/AnalizatzaileSintaktikoa.java
@@ -0,0 +1,14 @@
+package praktika1;
+
+import java.io.*;
+
+public class AnalizatzaileSintaktikoa {
+
+public int M
+{ int erreferentzia;
+	M_erref= lortu_erreferentzia();
+	return M_erref;
+}
+
+}
+}
