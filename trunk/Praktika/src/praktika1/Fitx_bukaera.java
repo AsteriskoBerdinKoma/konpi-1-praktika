6521c768a36b5ae59fc0e6b8628befe1540c8aaa
blizarazu <blizarazu@c955568d-1241-0410-a7ed-d13cdb9948ed>
2007-12-12 08:30:50 +0000

    Initial import.

---
+++ trunk/Praktika/src/praktika1/Fitx_bukaera.java
@@ -0,0 +1,16 @@
+package praktika1;
+
+
+///////////////////////////////////////////////////
+// Praktika Raul Barrenak partzialki euskaratuta //
+///////////////////////////////////////////////////
+
+public class Fitx_bukaera extends Exception{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Fitx_bukaera() {
+    }
+}
